cb35abae996e63d529655464c13f5b30934eb52b
Sstark97 <aitorscinfo@gmail.com>
2021-08-07 14:00:39 +0100

    Fix some bugs in useGetRandomCountry and add Data folder

---
+++ example.js
@@ -0,0 +1,3 @@
+import { callingCodeData } from "./src/data/callingCodeData.json";
+
+console.log(callingCodeData);
--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './assets/styles/App.css';
+import useGetRandomCountry from './hooks/useGetRandomCountry';
 
 function App() {
+  const country = useGetRandomCountry().response;
+  const loading = useGetRandomCountry().loading;
+  
+  if(!loading){
+    console.log(country);
+  }
+  
+
   return (
     <div className="App">
-      <h2>Country-Quiz</h2>
+      <h2 id="country-quiz">Country-Quiz</h2>
     </div>
   );
 }
